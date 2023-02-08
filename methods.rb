86173389b6e4268be3b41fde9e18767aa460e7d3
phylis13 <phylisnjeri54@gmail.com>
2023-02-08 08:19:55 +0300

    done

--- methods.rb
+++ methods.rb
@@ -1,1 +1,30 @@
-# Your code here!
+# Your  puts "Hello, " + 
+def greet_programmer
+    puts "Hello, programmer!"
+  end
+  
+  
+
+def greet_with_default(name = "programmer!")
+    puts "Hello, #{name}!"
+  end
+  
+
+def greet(name)
+        puts "Hello, #{name}!"
+        end
+        
+        greet("Naureen")
+
+def greetWithDefault(name = "Sunny!")
+            puts "Hello, #{name}!"
+          end
+ def add(num1, num2)
+            num1 + num2
+          end
+ def halve(number)
+  return nil unless number.is_a? Numeric
+          
+      number / 2
+     end
+                              
